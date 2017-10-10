d7928e65a037d23bbccf69818c591f1a506dcb0e
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2017-10-10 17:57:59 +0200

    Implementing flattened array return in genScan instead of text file creation

--- include/xhal/rpc/calibration_routines.h
+++ include/xhal/rpc/calibration_routines.h
@@ -4,6 +4,6 @@
 #include "xhal/rpc/utils.h"
 
 DLLEXPORT uint32_t ttcGenConf(uint32_t L1Ainterval, uint32_t pulseDelay);
-DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t enCal, uint32_t mask, char * scanReg);
+DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t enCal, uint32_t mask, char * scanReg, uint32_t * result);
 
 #endif
