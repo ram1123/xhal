ff883df1b32ab525ea678ec3c6f3f80a123726c6
Brian Dorney <brian.l.dorney@cern.ch>
2017-10-13 14:25:56 +0200

    Fixed compile issue

--- src/common/rpc_manager/optohybrid.cc
+++ src/common/rpc_manager/optohybrid.cc
@@ -3,8 +3,8 @@
 DLLEXPORT uint32_t broadcastRead(uint32_t ohN, char * regName, uint32_t vfatMask, uint32_t * result){
     req = wisc::RPCMsg("optohybrid.broadcastRead");
 
+    req.set_string("reg_name",std::string(regName));
     req.set_word("oh_number",ohN);
-    req.set_word("reg_name",regName);
     req.set_word("mask",vfatMask);
     wisc::RPCSvc* rpc_loc = getRPCptr();
 
@@ -18,6 +18,7 @@ DLLEXPORT uint32_t broadcastRead(uint32_t ohN, char * regName, uint32_t vfatMask
         return 1;
     }
     else if (rsp.get_key_exists("data")) {
+        const uint32_t size = 24;
         ASSERT(rsp.get_word_array_size("data") == size);
         rsp.get_word_array("data", result);
     } else {
@@ -30,8 +31,8 @@ DLLEXPORT uint32_t broadcastRead(uint32_t ohN, char * regName, uint32_t vfatMask
 DLLEXPORT uint32_t broadcastWrite(uint32_t ohN, char * regName, uint32_t value, uint32_t vfatMask){
     req = wisc::RPCMsg("optohybrid.broadcastWrite");
 
+    req.set_string("reg_name",std::string(regName));
     req.set_word("oh_number",ohN);
-    req.set_word("reg_name",regName);
     req.set_word("value",value);
     req.set_word("mask",vfatMask);
     wisc::RPCSvc* rpc_loc = getRPCptr();
