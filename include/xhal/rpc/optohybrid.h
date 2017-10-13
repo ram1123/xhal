1344ba5a7b0871b52b776eb97265b901b268a96b
Mykhailo Dalchenko <mexanick@users.noreply.github.com>
2017-10-13 15:47:28 +0200

    Revert "New Feature: Interface for broadcastRead & Write"

--- include/xhal/rpc/optohybrid.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPTOHYBRID_H
-#define OPTOHYBRID_H
-
-#include "xhal/rpc/utils.h"
-
-DLLEXPORT uint32_t broadcastRead(uint32_t ohN, char * regName, uint32_t vfatMask, uint32_t * result);
-DLLEXPORT uint32_t broadcastWrite(uint32_t ohN, char * regName, uint32_t value, uint32_t vfatMask);
-
-#endif
