d4152b6181109df0c882b1c49657005aad9f02be
Brian Dorney <brian.l.dorney@cern.ch>
2017-11-02 10:00:25 +0100

    updated calibration_routines.*

--- src/common/rpc_manager/calibration_routines.cc
+++ src/common/rpc_manager/calibration_routines.cc
@@ -1,16 +1,37 @@
 #include "xhal/rpc/calibration_routines.h"
 
+DLLEXPORT uint32_t ttcGenToggle(uint32_t ohN, bool bRun){
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+    req = wisc::RPCMsg("calibration_routines.ttcGenToggle");
+    req.set_word("ohN", ohN);
+    req.set_word("bRun", bRun);
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        return 1;
+    }
+    return 0;
+} //End ttcGenToggle(...)
+
 /***
  * @brief configure TTC generator
  */
-DLLEXPORT uint32_t ttcGenConf(uint32_t L1Ainterval, uint32_t pulseDelay)
+DLLEXPORT uint32_t ttcGenConf(uint32_t ohN, uint32_t mode, uint32_t type, uint32_t pulseDelay, uint32_t L1Ainterval, uint32_t nPulses, bool bRun)
 {
     wisc::RPCSvc* rpc_loc = getRPCptr();
     //wisc::RPCMsg req_loc, rsp_loc;
     //req_loc = wisc::RPCMsg("calibration_routines.ttcGenConf");
     req = wisc::RPCMsg("calibration_routines.ttcGenConf");
-    req.set_word("L1Ainterval", L1Ainterval);
+    req.set_word("ohN", ohN);
+    req.set_word("mode", mode);
+    req.set_word("type", type);
     req.set_word("pulseDelay", pulseDelay);
+    req.set_word("L1Ainterval", L1Ainterval);
+    req.set_word("nPulses", nPulses);
+    req.set_word("bRun", bRun);
     try {
         rsp = rpc_loc->call_method(req);
     }
@@ -25,7 +46,7 @@ DLLEXPORT uint32_t ttcGenConf(uint32_t L1Ainterval, uint32_t pulseDelay)
 /***
  * @brief run a generic scan routine
  */
-DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t enCal, uint32_t mask, char * scanReg, uint32_t * result)
+DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t enCal, uint32_t mask, char * scanReg, bool useUltra, uint32_t * result)
 {
     req = wisc::RPCMsg("calibration_routines.genScan");
 
@@ -37,9 +58,11 @@ DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32
     req.set_word("ch", ch);
     req.set_word("enCal", enCal);
     req.set_word("mask", mask);
+    if(useUltra){
+        req.set_word("useUltra", useUltra);
+    }
     req.set_string("scanReg", std::string(scanReg));
 
-    //std::shared_ptr<wisc::RPCSvc> rpc_loc = getRPCptr();
     wisc::RPCSvc* rpc_loc = getRPCptr();
 
     try {
@@ -52,30 +75,13 @@ DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32
         return 1;
     }
     const uint32_t size = (dacMax - dacMin+1)*24/dacStep;
-    //uint32_t* result = new uint32_t[size];
     if (rsp.get_key_exists("data")) {
         ASSERT(rsp.get_word_array_size("data") == size);
-        rsp.get_word_array("data", result); 
+        rsp.get_word_array("data", result);
     } else {
         printf("No data key found");
         return 1;
-    } 
-    /*
-    FILE *f = fopen("file.txt", "w");
-    if (f == NULL)
-    {
-        printf("Error opening file!\n");
-        exit(1);
     }
-    fprintf(f, "vfatN/I:nEvents/I:dacVal/I:nHits/I\n");
-    uint32_t vfatSize = (dacMax-dacMin+1)/dacStep;
-    for (unsigned int i = 0; i<24; i++)
-    {
-        for (unsigned int j = 0; j<vfatSize; j++)
-        {
-            fprintf(f,"%d    %d    %d    %d\n",i,result[i*vfatSize+j]&0xFFFF,dacMin+j*dacStep,(result[i*vfatSize+j]>>16)&0xFFFF);
-        } 
-    }
-    */
+
     return 0;
 }
