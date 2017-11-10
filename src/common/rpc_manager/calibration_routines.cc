1892bef130f0c0b35eba94149a0c1501c84992eb
Brian Dorney <brian.l.dorney@cern.ch>
2017-11-10 16:37:38 +0100

    changed data types of calibration routines to be more intuitive

--- src/common/rpc_manager/calibration_routines.cc
+++ src/common/rpc_manager/calibration_routines.cc
@@ -70,7 +70,7 @@ DLLEXPORT uint32_t ttcGenConf(uint32_t ohN, uint32_t mode, uint32_t type, uint32
 /***
  * @brief run a generic scan routine
  */
-DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t useCalPulse, uint32_t mask, char * scanReg, bool useUltra, uint32_t * result)
+DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, bool useCalPulse, uint32_t mask, char * scanReg, bool useUltra, uint32_t * result)
 {
     req = wisc::RPCMsg("calibration_routines.genScan");
 
