dbb8770c21c73470722f5563f7d1b45419e8274c
Brian Dorney <brian.l.dorney@cern.ch>
2018-01-15 15:47:53 +0100

    added xhal routines for sbit trigger rate scan

--- src/common/rpc_manager/calibration_routines.cc
+++ src/common/rpc_manager/calibration_routines.cc
@@ -110,3 +110,46 @@ DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32
 
     return 0;
 }
+
+DLLEXPORT uint32_t sbitRateScan(uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t maskOh, char * scanReg, uint32_t * resultDacVal, uint32_t * resultTrigRate){
+    req = wisc::RPCMsg("calibration_routines.sbitRateScan");
+
+
+    req.set_word("ohN", ohN);
+    req.set_word("maskOh", maskOh);
+    req.set_word("dacMin", dacMin);
+    req.set_word("dacMax", dacMax);
+    req.set_word("dacStep", dacStep);
+    req.set_word("ch", ch);
+    req.set_string("scanReg", std::string(scanReg));
+
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+    const uint32_t size = (dacMax - dacMin+1)/dacStep;
+    if (rsp.get_key_exists("data_dacVal")) {
+        ASSERT(rsp.get_word_array_size("data_dacVal") == size);
+        rsp.get_word_array("data_dacVal", resultDacVal);
+    } else {
+        printf("No key found for data dac values");
+        return 1;
+    }
+
+    if (rsp.get_key_exists("data_trigRate")) {
+        ASSERT(rsp.get_word_array_size("data_trigRate") == size);
+        rsp.get_word_array("data_trigRate", resultTrigRate);
+    } else {
+        printf("No key found for data trigger rate values");
+        return 1;
+    }
+
+    return 0;
+} //End sbitRateScan(...)
