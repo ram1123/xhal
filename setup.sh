82ec6e840acc79ec1184c0c39f9974e2076afae3
Jared Sturdy <jsturdy@users.noreply.github.com>
2018-01-23 13:51:02 +0100

    Update setup.sh
    Make sourcing from outside the repo base stable

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-export XHAL_ROOT=$PWD
+export XHAL_ROOT=$(dirname "${BASH_SOURCE[0]}")
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XHAL_ROOT/lib
 export PATH=$PATH:$XHAL_ROOT/python/reg_interface
-# echo $XHAL_ROOT
