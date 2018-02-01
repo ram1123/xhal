00517125f47c139d9e42515acab798d5362b4ee6
Mykhailo Dalchenko <mexanick@users.noreply.github.com>
2018-02-01 17:10:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ then
     export XHAL_ROOT=$(dirname $(readlink -f "$0") )
 else
     # bash specific, must be sourced from a bash shell...                                                                                                                                                                                                                         
-    export XHAL_ROOT=$(dirname "${BASH_SOURCE[0]}")
+    export XHAL_ROOT=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 fi
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XHAL_ROOT/lib
