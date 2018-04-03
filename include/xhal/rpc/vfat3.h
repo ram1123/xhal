44835b7f02b25d833a0cf17aed6652578f296ff1
Brian Dorney <brian.l.dorney@cern.ch>
2018-04-03 17:19:35 +0200

    xhal functionality for writing channel registers

--- include/xhal/rpc/vfat3.h
+++ include/xhal/rpc/vfat3.h
@@ -5,4 +5,17 @@
 
 DLLEXPORT uint32_t configureVFAT3s(uint32_t ohN, uint32_t vfatMask);
 
+/*! \fn DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol)
+ *  \brief sets all vfat3 channel registers
+ *  \param ohN Optohybrid optical link number
+ *  \param vfatMask Bitmask of chip positions determining which chips to use
+ *  \param calEnable array pointer for calEnable with 3072 entries, the (vfat,chan) pairing determines the array index via: idx = vfat*128 + chan
+ *  \param masks as calEnable but for channel masks
+ *  \param trimARM as calEnable but for arming comparator trim value
+ *  \param trimARMPol as calEnable but for arming comparator trim polarity
+ *  \param trimZCC as calEnable but for zero crossing comparator trim value
+ *  \param trimZCCPol as calEnable but for zero crossing comparator trim polarity
+ */
+DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol);
+
 #endif
--- src/common/rpc_manager/vfat3.cc
+++ src/common/rpc_manager/vfat3.cc
@@ -1,3 +1,4 @@
+#include <array>
 #include "xhal/rpc/vfat3.h"
 
 /***
@@ -9,6 +10,44 @@ DLLEXPORT uint32_t configureVFAT3s(uint32_t ohN, uint32_t vfatMask)
 
     req.set_word("vfatMask",vfatMask);
     req.set_word("ohN",ohN);
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+    return 0;
+}
+
+/*! \fn DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol)
+ *  \brief sets all vfat3 channel registers
+ *  \param ohN Optohybrid optical link number
+ *  \param vfatMask Bitmask of chip positions determining which chips to use
+ *  \param calEnable array pointer for calEnable with 3072 entries, the (vfat,chan) pairing determines the array index via: idx = vfat*128 + chan
+ *  \param masks as calEnable but for channel masks
+ *  \param trimARM as calEnable but for arming comparator trim value
+ *  \param trimARMPol as calEnable but for arming comparator trim polarity
+ *  \param trimZCC as calEnable but for zero crossing comparator trim value
+ *  \param trimZCCPol as calEnable but for zero crossing comparator trim polarity
+ */
+DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol){
+    req = wisc::RPCMsg("vfat3.setChannelRegistersVFAT3");
+
+    req.set_word("vfatMask",vfatMask);
+    req.set_word("ohN",ohN);
+
+    req.set_word_array("calEnable",calEnable,3072);
+    req.set_word_array("masks",masks,3072);
+    req.set_word_array("trimARM",masks,3072);
+    req.set_word_array("trimARMPol",masks,3072);
+    req.set_word_array("trimZCC",masks,3072);
+    req.set_word_array("trimZCCPol",masks,3072);
+
     wisc::RPCSvc* rpc_loc = getRPCptr();
 
     try {
