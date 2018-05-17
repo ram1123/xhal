f9ad5ee18872ebe0311bae080836a931dd4347a5
Mykhailo Dalchenko <mexanick@users.noreply.github.com>
2018-05-17 19:03:18 +0200

    Update reg_interface.py

--- python/reg_interface/reg_interface.py
+++ python/reg_interface/reg_interface.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option("-e", "--execute", type="str", dest="exe",
-                      help="Function to execute once", metavar="exe", default=None)
+                      help="Function to execute once, should be used as reg_interface -e <method> \"<method arguments>\" - note the quotes", metavar="exe", default=None)
     parser.add_option("-n", "--hostname", type="string", dest="hostname",
                       help="CTP7 hostname, default is the one used at p5", default="")
 
