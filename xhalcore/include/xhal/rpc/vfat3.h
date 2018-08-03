2eb4bcd4be5dae6f829b2afd32746b90e3ae0d8c
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-03 18:10:07 +0200

    adding simpler method for setting channel registers

--- xhalcore/include/xhal/rpc/vfat3.h
+++ xhalcore/include/xhal/rpc/vfat3.h
@@ -26,4 +26,12 @@ DLLEXPORT uint32_t getChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uin
  */
 DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol);
 
+/*! \fn DLLEXPORT uint32_t setChannelRegistersVFAT3Simple(uint32_t ohN, uint32_t vfatMask, uint32_t *chanRegData)
+ *  \brief sets all vfat3 channel registers using a single channel register array
+ *  \param ohN Optohybrid optical link number
+ *  \param vfatMask Bitmask of chip positions determining which chips to use
+ *  \param chanRegData array pointer for channel register data with 3072 entries, the (vfat,chan) pairing determines the array index via: idx = vfat*128 + chan
+ */
+DLLEXPORT uint32_t setChannelRegistersVFAT3Simple(uint32_t ohN, uint32_t vfatMask, uint32_t *chanRegData);
+
 #endif
