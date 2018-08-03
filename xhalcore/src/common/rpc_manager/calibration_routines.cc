1e91567aae66c757b3f70c4ac23a1921329c8db3
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-03 18:36:31 +0200

    added function for checkSbitRateWithCalPulse

--- xhalcore/src/common/rpc_manager/calibration_routines.cc
+++ xhalcore/src/common/rpc_manager/calibration_routines.cc
@@ -1,5 +1,60 @@
 #include "xhal/rpc/calibration_routines.h"
 
+DLLEXPORT uint32_t checkSbitMappingWithCalPulse(uint32_t ohN, uint32_t mask, uint32_t currentPulse, uint32_t calScaleFactor, uint32_t nevts, uint32_t L1Ainterval, uint32_t pulseDelay, uint32_t *data){
+
+} //End checkSbitMappingWithCalPulse()
+
+DLLEXPORT uint32_t checkSbitRateWithCalPulse(uint32_t ohN, uint32_t mask, uint32_t currentPulse, uint32_t calScaleFactor, uint32_t waitTime, uint32_t pulseRate, uint32_t pulseDelay, uint32_t *outDataCTP7Rate, uint32_t *outDataFPGAClusterCntRate, uint32_t *outDataVFATSBits){
+    req = wisc::RPCMsg("calibration_routines.checkSbitRateWithCalPulse");
+
+    req.set_word("ohN", ohN);
+    req.set_word("mask", mask);
+    req.set_word("currentPulse", currentPulse);
+    req.set_word("calScaleFactor", calScaleFactor);
+    req.set_word("waitTime", waitTime);
+    req.set_word("pulseRate", pulseRate);
+    req.set_word("pulseDelay", pulseDelay);
+
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+
+    const uint32_t size = 128*24;
+    if (rsp.get_key_exists("outDataCTP7Rate")) {
+        ASSERT(rsp.get_word_array_size("outDataCTP7Rate") == size);
+        rsp.get_word_array("outDataCTP7Rate", outDataCTP7Rate);
+    }
+    else{
+        printf("No key found for outDataCTP7Rate");
+        return 1;
+    }
+
+    if (rsp.get_key_exists("outDataFPGAClusterCntRate")) {
+        ASSERT(rsp.get_word_array_size("outDataFPGAClusterCntRate") == size);
+        rsp.get_word_array("outDataFPGAClusterCntRate", outDataFPGAClusterCntRate);
+    }
+    else{
+        printf("No key found for outDataFPGAClusterCntRate");
+        return 1;
+    }
+
+    if (rsp.get_key_exists("outDataVFATSBits")) {
+        ASSERT(rsp.get_word_array_size("outDataVFATSBits") == size);
+        rsp.get_word_array("outDataVFATSBits", outDataVFATSBits);
+    }
+    else{
+        printf("No key found for outDataVFATSBits");
+        return 1;
+    }
+
+    return 0;
+} //End checkSbitRateWithCalPulse()
 
 /***
  * @brief run a generic scan routine for a specific channel
