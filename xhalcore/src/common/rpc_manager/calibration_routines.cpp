43c4c217e268ed701fd6f6de701a91aca4ed6502
Mykhailo Dalchenko <mykhailo.dalchenko@cern.ch>
2018-08-22 09:41:17 +0200

    Refactoring calibration routines

--- xhalcore/src/common/rpc_manager/calibration_routines.cpp
+++ xhalcore/src/common/rpc_manager/calibration_routines.cpp
@@ -1,6 +1,7 @@
 #include "xhal/rpc/calibration_routines.h"
 
-DLLEXPORT uint32_t checkSbitMappingWithCalPulse(uint32_t ohN, uint32_t vfatN, uint32_t mask, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, uint32_t nevts, uint32_t L1Ainterval, uint32_t pulseDelay, uint32_t *data){
+uint32_t xhal::rpc::CalRoutines::checkSbitMappingWithCalPulse(uint32_t ohN, uint32_t vfatN, uint32_t mask, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, uint32_t nevts, uint32_t L1Ainterval, uint32_t pulseDelay, uint32_t *data)
+{
     req = wisc::RPCMsg("calibration_routines.checkSbitMappingWithCalPulse");
 
     req.set_word("ohN", ohN);
@@ -13,10 +14,8 @@ DLLEXPORT uint32_t checkSbitMappingWithCalPulse(uint32_t ohN, uint32_t vfatN, ui
     req.set_word("L1Ainterval", L1Ainterval);
     req.set_word("pulseDelay", pulseDelay);
 
-    wisc::RPCSvc* rpc_loc = getRPCptr();
-
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
@@ -38,7 +37,8 @@ DLLEXPORT uint32_t checkSbitMappingWithCalPulse(uint32_t ohN, uint32_t vfatN, ui
     return 0;
 } //End checkSbitMappingWithCalPulse()
 
-DLLEXPORT uint32_t checkSbitRateWithCalPulse(uint32_t ohN, uint32_t vfatN, uint32_t mask, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, uint32_t waitTime, uint32_t pulseRate, uint32_t pulseDelay, uint32_t *outDataCTP7Rate, uint32_t *outDataFPGAClusterCntRate, uint32_t *outDataVFATSBits){
+uint32_t xhal::rpc::CalRoutines::checkSbitRateWithCalPulse(uint32_t ohN, uint32_t vfatN, uint32_t mask, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, uint32_t waitTime, uint32_t pulseRate, uint32_t pulseDelay, uint32_t *outDataCTP7Rate, uint32_t *outDataFPGAClusterCntRate, uint32_t *outDataVFATSBits)
+{
     req = wisc::RPCMsg("calibration_routines.checkSbitRateWithCalPulse");
 
     req.set_word("ohN", ohN);
@@ -51,10 +51,8 @@ DLLEXPORT uint32_t checkSbitRateWithCalPulse(uint32_t ohN, uint32_t vfatN, uint3
     req.set_word("pulseRate", pulseRate);
     req.set_word("pulseDelay", pulseDelay);
 
-    wisc::RPCSvc* rpc_loc = getRPCptr();
-
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
@@ -94,10 +92,7 @@ DLLEXPORT uint32_t checkSbitRateWithCalPulse(uint32_t ohN, uint32_t vfatN, uint3
     return 0;
 } //End checkSbitRateWithCalPulse()
 
-/***
- * @brief run a generic scan routine for a specific channel
- */
-DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, uint32_t mask, char * scanReg, bool useUltra, bool useExtTrig, uint32_t * result)
+uint32_t xhal::rpc::CalRoutines::genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, uint32_t mask, char * scanReg, bool useUltra, bool useExtTrig, uint32_t * result)
 {
     req = wisc::RPCMsg("calibration_routines.genScan");
 
@@ -117,10 +112,8 @@ DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32
     req.set_word("useExtTrig", useExtTrig);
     req.set_string("scanReg", std::string(scanReg));
 
-    wisc::RPCSvc* rpc_loc = getRPCptr();
-
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
@@ -140,10 +133,8 @@ DLLEXPORT uint32_t genScan(uint32_t nevts, uint32_t ohN, uint32_t dacMin, uint32
     return 0;
 }
 
-/***
- * @brief run a generic scan routine on all channels
- */
-DLLEXPORT uint32_t genChannelScan(uint32_t nevts, uint32_t ohN, uint32_t mask, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, bool useExtTrig, char * scanReg, bool useUltra, uint32_t * result){
+uint32_t xhal::rpc::CalRoutines::genChannelScan(uint32_t nevts, uint32_t ohN, uint32_t mask, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, bool useCalPulse, bool currentPulse, uint32_t calScaleFactor, bool useExtTrig, char * scanReg, bool useUltra, uint32_t * result)
+{
     req = wisc::RPCMsg("calibration_routines.genChannelScan");
 
     req.set_word("nevts", nevts);
@@ -161,10 +152,8 @@ DLLEXPORT uint32_t genChannelScan(uint32_t nevts, uint32_t ohN, uint32_t mask, u
     }
     req.set_string("scanReg", std::string(scanReg));
 
-    wisc::RPCSvc* rpc_loc = getRPCptr();
-
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
@@ -184,7 +173,8 @@ DLLEXPORT uint32_t genChannelScan(uint32_t nevts, uint32_t ohN, uint32_t mask, u
     return 0;
 } //End genChannelScan()
 
-DLLEXPORT uint32_t sbitRateScan(uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t maskOh, bool invertVFATPos, char * scanReg, uint32_t waitTime, uint32_t * resultDacVal, uint32_t * resultTrigRate, uint32_t * resultTrigRatePerVFAT, bool isParallel){
+uint32_t xhal::rpc::CalRoutines::sbitRateScan(uint32_t ohN, uint32_t dacMin, uint32_t dacMax, uint32_t dacStep, uint32_t ch, uint32_t maskOh, bool invertVFATPos, char * scanReg, uint32_t waitTime, uint32_t * resultDacVal, uint32_t * resultTrigRate, uint32_t * resultTrigRatePerVFAT, bool isParallel)
+{
     req = wisc::RPCMsg("calibration_routines.sbitRateScan");
 
 
@@ -199,8 +189,6 @@ DLLEXPORT uint32_t sbitRateScan(uint32_t ohN, uint32_t dacMin, uint32_t dacMax, 
     req.set_string("scanReg", std::string(scanReg));
     req.set_word("waitTime", waitTime);
 
-    wisc::RPCSvc* rpc_loc = getRPCptr();
-
     //Check to make sure (dacMax-dacMin+1)/dacStep is an integer!
     if( 0 != ((dacMax - dacMin + 1) % dacStep) ){
         printf("Caught an error: (dacMax - dacMin + 1)/dacStep must be an integer!\n");
@@ -209,7 +197,7 @@ DLLEXPORT uint32_t sbitRateScan(uint32_t ohN, uint32_t dacMin, uint32_t dacMax, 
     const uint32_t size = (dacMax - dacMin+1)/dacStep;
 
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
@@ -248,33 +236,9 @@ DLLEXPORT uint32_t sbitRateScan(uint32_t ohN, uint32_t dacMin, uint32_t dacMax, 
     return 0;
 } //End sbitRateScan(...)
 
-/***
- * @brief configure TTC generator
- */
-DLLEXPORT uint32_t ttcGenConf(uint32_t ohN, uint32_t mode, uint32_t type, uint32_t pulseDelay, uint32_t L1Ainterval, uint32_t nPulses, bool enable)
+uint32_t xhal::rpc::CalRoutines::ttcGenConf(uint32_t ohN, uint32_t mode, uint32_t type, uint32_t pulseDelay, uint32_t L1Ainterval, uint32_t nPulses, bool enable)
 {
-    /*
-     * v3  electronics Behavior:
-     *      pulseDelay (only for enable = true), delay between CalPulse and L1A
-     *      L1Ainterval (only for enable = true), how often to repeat signals
-     *      enable = true (false) ignore (take) ttc commands from backplane for this AMC (affects all links)
-     * v2b electronics behavior:
-     *      Configure the T1 controller
-     *      mode: 0 (Single T1 signal),
-     *            1 (CalPulse followed by L1A),
-     *            2 (pattern)
-     *      type (only for mode 0, type of T1 signal to send):
-     *            0 L1A
-     *            1 CalPulse
-     *            2 Resync
-     *            3 BC0
-     *      pulseDelay (only for mode 1), delay between CalPulse and L1A
-     *      L1Ainterval (only for mode 0,1), how often to repeat signals
-     *      nPulses how many signals to send (0 is continuous)
-     *      enable = true (false) start (stop) the T1Controller for link ohN
-     */
 
-    wisc::RPCSvc* rpc_loc = getRPCptr();
     req = wisc::RPCMsg("calibration_routines.ttcGenConf");
     req.set_word("ohN", ohN);
     req.set_word("mode", mode);
@@ -284,7 +248,7 @@ DLLEXPORT uint32_t ttcGenConf(uint32_t ohN, uint32_t mode, uint32_t type, uint32
     req.set_word("nPulses", nPulses);
     req.set_word("enable", enable);
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
@@ -294,18 +258,13 @@ DLLEXPORT uint32_t ttcGenConf(uint32_t ohN, uint32_t mode, uint32_t type, uint32
     return 0;
 }
 
-DLLEXPORT uint32_t ttcGenToggle(uint32_t ohN, bool enable){
-    /*
-     * v3  electronics: enable = true (false) ignore (take) ttc commands from backplane for this AMC
-     * v2b electronics: enable = true (false) start (stop) the T1Controller for link ohN
-     */
-
-    wisc::RPCSvc* rpc_loc = getRPCptr();
+uint32_t xhal::rpc::CalRoutines::ttcGenToggle(uint32_t ohN, bool enable)
+{
     req = wisc::RPCMsg("calibration_routines.ttcGenToggle");
     req.set_word("ohN", ohN);
     req.set_word("enable", enable);
     try {
-        rsp = rpc_loc->call_method(req);
+        rsp = rpc.call_method(req);
     }
     STANDARD_CATCH;
 
