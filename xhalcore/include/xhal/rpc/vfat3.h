2a55696ff54aa38f7647cfdaa77d96f52b7d943b
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-22 16:22:52 +0200

    added functionality for configuring DAC Monitoring for VFAT3s

--- xhalcore/include/xhal/rpc/vfat3.h
+++ xhalcore/include/xhal/rpc/vfat3.h
@@ -5,6 +5,22 @@
 
 DLLEXPORT uint32_t configureVFAT3s(uint32_t ohN, uint32_t vfatMask);
 
+/*! \fn DLLEXPORT uint32_t configureVFAT3DacMonitor(uint32_t ohN, uint32_t vfatMask, uint32_t dacSelect)
+ *  \brief configures all unmasked VFATs on ohN for ADC Monitoring of the DAC specified by dacSelect.  See the VFAT3 manual for possible values of dacSelect.
+ *  \param ohN Optohybrid optical link number
+ *  \param vfatMask Bitmask of chip positions determining which chips to use
+ *  \param dacSelect the monitoring selection for the VFAT3 ADC, possible values are [0,16] and [32,41].  See VFAT3 manual for details
+ */
+DLLEXPORT uint32_t configureVFAT3DacMonitor(uint32_t ohN, uint32_t vfatMask, uint32_t dacSelect);
+
+/*! \fn DLLEXPORT uint32_t configureVFAT3DacMonitorMultiLink(uint32_t ohMask, uint32_t *ohVfatMaskArray, uint32_t dacSelect)
+ *  \brief configures all unmasked VFATs on ohN for ADC Monitoring of the DAC specified by dacSelect.  See the VFAT3 manual for possible values of dacSelect.
+ *  \param ohMask A 12 bit number which specifies which optohybrids to read from.  Having a value of 1 in the n^th bit indicates that the n^th optohybrid should be considered.
+ *  \param ohVfatMaskArray Pointer to an array of length 12.  Each element is the bitmask of chip positions determining which chips to use for the optohybrid number corresponding to the element index.
+ *  \param dacSelect the monitoring selection for the VFAT3 ADC, possible values are [0,16] and [32,41].  See VFAT3 manual for details
+ */
+DLLEXPORT uint32_t configureVFAT3DacMonitorMultiLink(uint32_t ohMask, uint32_t *ohVfatMaskArray, uint32_t dacSelect);
+
 /*! \fn DLLEXPORT uint32_t getChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *chanRegData);
  *  \brief reads channel registers of all unmasked vfats on ohN
  *  \param ohN Optohybrid optical link number
@@ -12,6 +28,24 @@ DLLEXPORT uint32_t configureVFAT3s(uint32_t ohN, uint32_t vfatMask);
  *  \param chanRegData array pointer for channel register data with 3072 entries, the (vfat,chan) pairing determines the array index via: idx = vfat*128 + chan
  */
 DLLEXPORT uint32_t getChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *chanRegData);
+
+/*! \fn DLLEXPORT uint32_t readVFAT3ADC(uint32_t ohN, uint32_t *adcData, bool useExtRefADC=false, uint32_t vfatMask=0xFF000000)
+ *  \brief Reads the ADC value from all unmasked VFATs
+ *  \param adcData pointer to the array containing the ADC results; length of array is expected to be 24
+ *  \param ohN Optohybrid optical link number
+ *  \param useExtRefADC true (false) read the ADC1 (ADC0) which uses an external (internal) reference
+ *  \param vfatMask Bitmask of chip positions determining which chips to use
+ */
+DLLEXPORT uint32_t readVFAT3ADC(uint32_t ohN, uint32_t *adcData, bool useExtRefADC=false, uint32_t vfatMask=0xFF000000);
+
+/*! \fn DLLEXPORT uint32_t readVFAT3ADCMultiLink(uint32_t ohN, uint32_t *adcData, bool useExtRefADC=false, uint32_t vfatMask=0xFF000000)
+ *  \brief As readVFAT3ADC(...) but for all optical links specified in ohMask on the AMC
+ *  \param ohMask A 12 bit number which specifies which optohybrids to read from.  Having a value of 1 in the n^th bit indicates that the n^th optohybrid should be considered.
+ *  \param ohVfatMaskArray Pointer to an array of length 12.  Each element is the bitmask of chip positions determining which chips to use for the optohybrid number corresponding to the element index.
+ *  \param adcDataAll pointer to the array containing the ADC results; length of array is expected to be 12*24=288
+ *  \param useExtRefADC true (false) read the ADC1 (ADC0) which uses an external (internal) reference
+ */
+DLLEXPORT uint32_t readVFAT3ADCMultiLink(uint32_t ohMask, uint32_t *ohVfatMaskArray, uint32_t *adcDataAll, bool useExtRefADC=false);
 
 /*! \fn DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol)
  *  \brief sets all vfat3 channel registers
--- xhalcore/src/common/rpc_manager/vfat3.cc
+++ xhalcore/src/common/rpc_manager/vfat3.cc
@@ -24,6 +24,46 @@ DLLEXPORT uint32_t configureVFAT3s(uint32_t ohN, uint32_t vfatMask)
     return 0;
 }
 
+DLLEXPORT uint32_t configureVFAT3DacMonitor(uint32_t ohN, uint32_t vfatMask, uint32_t dacSelect){
+    req = wisc::RPCMsg("vfat3.configureVFAT3DacMonitor");
+
+    req.set_word("ohN",ohN);
+    req.set_word("vfatMask",vfatMask);
+    req.set_word("dacSelect",dacSelect);
+
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+    return 0;
+} //End configureVFAT3DacMonitor(...)
+
+DLLEXPORT uint32_t configureVFAT3DacMonitorMultiLink(uint32_t ohMask, uint32_t *ohVfatMaskArray, uint32_t dacSelect){
+    req = wisc::RPCMsg("vfat3.configureVFAT3DacMonitorMultiLink");
+
+    req.set_word("ohMask",ohMask);
+    req.set_word_array("ohVfatMaskArray",ohVfatMaskArray,12);
+    req.set_word("dacSelect",dacSelect);
+
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+    return 0;
+} //End configureVFAT3DacMonitor(...)
+
 DLLEXPORT uint32_t getChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *chanRegData){
     req = wisc::RPCMsg("vfat3.getChannelRegistersVFAT3");
 
@@ -53,6 +93,66 @@ DLLEXPORT uint32_t getChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uin
 
     return 0;
 }
+
+DLLEXPORT uint32_t readVFAT3ADC(uint32_t ohN, uint32_t *adcData, bool useExtRefADC, uint32_t vfatMask){
+    req = wisc::RPCMsg("vfat3.readVFAT3ADC");
+
+    req.set_word("ohN",ohN);
+    req.set_word("useExtRefADC", useExtRefADC);
+    req.set_word("vfatMask",vfatMask);
+
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+
+    const uint32_t size = 24;
+    if (rsp.get_key_exists("adcData")) {
+        ASSERT(rsp.get_word_array_size("adcData") == size);
+        rsp.get_word_array("adcData", adcData);
+    } else {
+        printf("No channel register data found");
+        return 1;
+    }
+
+    return 0;
+} //End readVFAT3ADC(...)
+
+DLLEXPORT uint32_t readVFAT3ADCMultiLink(uint32_t ohMask, uint32_t *ohVfatMaskArray, uint32_t *adcDataAll, bool useExtRefADC){
+    req = wisc::RPCMsg("vfat3.readVFAT3ADCMultiLink");
+
+    req.set_word("ohMask",ohMask);
+    req.set_word_array("ohVfatMaskArray",ohVfatMaskArray, 12);
+    req.set_word("useExtRefADC", useExtRefADC);
+
+    wisc::RPCSvc* rpc_loc = getRPCptr();
+    try {
+        rsp = rpc_loc->call_method(req);
+    }
+    STANDARD_CATCH;
+
+    if (rsp.get_key_exists("error")) {
+        printf("Caught an error: %s\n", (rsp.get_string("error")).c_str());
+        return 1;
+    }
+
+    const uint32_t size = 12*24;
+    if (rsp.get_key_exists("adcDataAll")) {
+        ASSERT(rsp.get_word_array_size("adcDataAll") == size);
+        rsp.get_word_array("adcDataAll", adcDataAll);
+    } else {
+        printf("No channel register data found");
+        return 1;
+    }
+
+    return 0;
+} //End readVFAT3ADCMultiLink(...)
 
 DLLEXPORT uint32_t setChannelRegistersVFAT3(uint32_t ohN, uint32_t vfatMask, uint32_t *calEnable, uint32_t *masks, uint32_t *trimARM, uint32_t *trimARMPol, uint32_t *trimZCC, uint32_t *trimZCCPol){
     req = wisc::RPCMsg("vfat3.setChannelRegistersVFAT3");
