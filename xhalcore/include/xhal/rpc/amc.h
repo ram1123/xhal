077be7a2c43082c874afa76c3fd083efeee09eab
Brian Dorney <brian.l.dorney@cern.ch>
2018-08-22 16:21:50 +0200

    updated function documentation

--- xhalcore/include/xhal/rpc/amc.h
+++ xhalcore/include/xhal/rpc/amc.h
@@ -7,6 +7,7 @@
  *  \brief SBIT readout from optohybrid ohN for a number of seconds given by acquireTime; data is written to a file directory specified by outFilePath
  *  \param ohN Optohybrid optical link number
  *  \param acquireTime acquisition time on the wall clock in seconds
+ *  \return An integer from the set {0, 1, EIO}; where 0 indicates successful completion, 1 indicates an RPC error, and EIO is a platform dependent error code representing a file IO error
  */
 DLLEXPORT uint32_t sbitReadOut(uint32_t ohN, uint32_t acquireTime, char * outFilePath);
 
