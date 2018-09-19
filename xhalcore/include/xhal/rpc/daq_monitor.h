d5f1d5e2be61e36cd2d95bd64d4d142beb138180
Brian Dorney <brian.l.dorney@cern.ch>
2018-09-19 10:53:25 +0200

    new struct for storing sysmon info plus ohMask functionality to monitoring functions

--- xhalcore/include/xhal/rpc/daq_monitor.h
+++ xhalcore/include/xhal/rpc/daq_monitor.h
@@ -6,7 +6,7 @@
 struct SCAMonitor{
     //Temperature Data
     uint32_t ohBoardTemp[9];
-    uint32_t ohFPGACoreTemp;
+    //uint32_t ohFPGACoreTemp;
     uint32_t scaTemp;
 
     //Voltages
@@ -22,13 +22,30 @@ struct SCAMonitor{
     uint32_t VTRX_RSSI1;
 };
 
-DLLEXPORT uint32_t getmonTTCmain(uint32_t* result);
-DLLEXPORT uint32_t getmonTRIGGERmain(uint32_t* result, uint32_t noh = 12);
-DLLEXPORT uint32_t getmonTRIGGEROHmain(uint32_t* result, uint32_t noh = 12);
-DLLEXPORT uint32_t getmonDAQmain(uint32_t* result);
-DLLEXPORT uint32_t getmonDAQOHmain(uint32_t* result, uint32_t noh = 12);
-DLLEXPORT uint32_t getmonOHmain(uint32_t* result, uint32_t noh = 12);
+struct SysmonMonitor{
+    //Alarms
+    bool isOverTemp;
+    bool isInVCCAuxAlarm;
+    bool isInVCCIntAlarm;
 
+    //Counters
+    uint32_t cntOverTemp;
+    uint32_t cntVCCAuxAlarm;
+    uint32_t cntVCCIntAlarm;
+
+    //Values
+    uint32_t fpgaCoreTemp;
+    uint32_t fpgaCore1V0;
+    uint32_t fpgaCore2V5_IO;
+};
+
+DLLEXPORT uint32_t getmonTTCmain(uint32_t* result);
+DLLEXPORT uint32_t getmonTRIGGERmain(uint32_t* result, uint32_t noh = 12, uint32_t ohMask = 0xfff);
+DLLEXPORT uint32_t getmonTRIGGEROHmain(uint32_t* result, uint32_t noh = 12, uint32_t ohMask = 0xff);
+DLLEXPORT uint32_t getmonDAQmain(uint32_t* result);
+DLLEXPORT uint32_t getmonDAQOHmain(uint32_t* result, uint32_t noh = 12, uint32_t ohMask = 0xfff);
+DLLEXPORT uint32_t getmonOHmain(uint32_t* result, uint32_t noh = 12, uint32_t ohMask = 0xfff);
 DLLEXPORT uint32_t getmonOHSCAmain(struct SCAMonitor *scaMon, uint32_t noh = 12, uint32_t ohMask = 0xfff);
+DLLEXPORT uint32_t getmonOHSysmon(struct SysmonMonitor *sysmon, uint32_t noh =12, uint32_t ohMask = 0xfff, bool doReset=false);
 
 #endif
